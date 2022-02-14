a922a5342438c46c06b9f6e3d4aca0cc083f9097
Didier <blessingmosley@hotmail.com>
2022-02-14 00:40:02 -0500

    last commit for now

---
+++ comunal.js
@@ -0,0 +1,10 @@
+// const pres = document.querySelector(".presentation");
+// let cont = 0;
+
+// while(true){
+//     setInterval(()=>{
+//         cont++;
+//         pres.style.background = `var(--gradient), url("./img/img${cont}.jpg");`;
+//         cont = cont > 3 ? 0 : cont;
+//     },500);
+// }
